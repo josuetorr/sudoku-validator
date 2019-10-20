71b6fd1553dc1a8ed4dab12262b79f9bd96ca3bc
josuetorr <josue.dtorres@gmail.com>
2019-10-19 21:38:00 -0400

    able to extract a matric

--- tp1.c
+++ tp1.c
@@ -7,10 +7,6 @@
 // nombre maximal de caractere quon peut lire
 #define MAX_INPUT_COUNT 1000
 
-// On doit compter les espaces entre chaque caractere ainse
-// que le caractere de retour (\n)
-#define DIMENSION_SUDOKU 18
-
 /**
  * Obtenir un pointeur de stream vers le fichier que nous voulons ouvrir
  *
@@ -31,7 +27,7 @@ FILE *get_fichier (const char chemin[])
 /**
  * Retourne le contenu du fichier en une chaine de caracteres
  */
-char *get_input (char str[], FILE *fichier)
+char *get_input_de_fichier (char str[], FILE *fichier)
 {
     char ligne[20];
     while (fgets(ligne, sizeof ligne, fichier))
@@ -42,7 +38,9 @@ char *get_input (char str[], FILE *fichier)
 
 /**
  * Extrait un sudoku d'une chaine de caracteres peut importe si la chaine est
- * un sudoku valide ou non.
+ * un sudoku valide ou non. Afin de synchroniser les coordonnes de la matrice,
+ * on simule la chaine "str" comme une matrice en utilisant la formule pour 
+ * indice = i*dimension + j, ou i et j sont les coordonnees de la matrice.
  *
  * @param hauteur   Hauteur de la matrice
  * @param longueur  Longueur de la matrice
@@ -58,44 +56,98 @@ void extraire_sudoku (size_t dimension, char sudoku[dimension][dimension], char 
         for (int j = 0; j < dimension; j++) {
 
             indice_str = i*dimension + j;
-            sudoku[i][j] = str[indice_str];
+            if (!isspace(str[indice_str]))
+                sudoku[i][j] = str[indice_str];
+
         }
     }
 }
 
+/**
+ * Extrait une sous-chaine de "str" a partir du debut de "str" 
+ * jusqu'a la position "fin". Cette fonction modifie le pointeur str.
+ * Afin de preserver la chaine de caractere, on suggere de copier str
+ * dans une autre chaine avec strcpy
+ *
+ * @param str   Chaine original
+ * @param fin   Derniere position de la sous-chaine
+ *
+ * @return  La sous-chaine correspondante. Si "fin" est plus grand
+ *          que la longueur de "str", alors on retourne NULL
+ */
+char *sub_str (const char *str, char* substr, int fin)
+{
+    if (fin > strlen(str)) 
+       return NULL;
+
+    for (int i = 0; i < fin; i++)
+        substr[i] = str[i];
+    substr[fin] = 0;
+
+    str = str + fin;
+
+    return substr;
+}
+
+/**
+ * Extraire tout les sudokus d'une chaine de caractere.
+ * Un sudoku est delimite par deux caracteres de retours consecutifs.
+ * Les sudokus seront stockes dans une liste ayant comme longueur le nombre
+ * de sudoku dans la chaine passee en parametre
+ *
+ * @param str   Chaine d'ou on veut extraire les sudokus
+ * @param liste Le tableau dans lequel les sudokus seront stockes
+ */
+void extraire_all_sudoku(char str[])
+{
+    const char *delim = "\n\n";
+    const char *pos_of_delim = strstr(str, delim);
+    char substr[pos_of_delim - str];
+    sub_str(str, substr, pos_of_delim - str);
+    
+    
+}
 
 void print_sudoku(size_t size, char sudoku[size][size])
 {
-    for (int i = 0; i < size; i++)
-        for(int j = 0; j < size; j++)
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
             printf("%c", sudoku[i][j]);
-
-    /* printf("\n"); */
+        }
+        printf("\n");
+    }
 }
 
 int main (int argc, char *argv[])
 {
     if (argc != 2) {
+
         fprintf(stderr, "%s: Usage: <nom-fichier>\n", argv[0]);
         exit(1);
+
     }
 
     FILE *fichier = get_fichier(argv[1]);
     if (!fichier) {
+
         fprintf(stderr, "%s: Erreur: Fichier \"%s\" n'existe pas\n", argv[0], argv[1]);
         exit(1);
+
     }
 
     // Lecture du fichie contenant les sudokus
     char entree_fichier[MAX_INPUT_COUNT] = {0};
-    get_input(entree_fichier, fichier);
+    get_input_de_fichier(entree_fichier, fichier);
     fclose(fichier);
 
-    char sudoku[DIMENSION_SUDOKU][DIMENSION_SUDOKU] = {0};
-    extraire_sudoku(DIMENSION_SUDOKU, sudoku, entree_fichier);
+    extraire_all_sudoku(entree_fichier);
+    printf("Original after sub_str:\n%s\n", entree_fichier);
 
-    /* printf("%s", entree_fichier); */
-    print_sudoku(DIMENSION_SUDOKU, sudoku);
+    /* char sudoku[DIMENSION_SUDOKU][DIMENSION_SUDOKU] = {0}; */
+    /* extraire_sudoku(DIMENSION_SUDOKU, sudoku, entree_fichier); */
+
+    /* /1* printf("%s", entree_fichier); *1/ */
+    /* print_sudoku(DIMENSION_SUDOKU, sudoku); */
 
     return 0;
 }
